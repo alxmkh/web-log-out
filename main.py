0d5b9f7f739af4b9ef3aeba3c6360087bc6340f3
alxmkh <molvinskih.a@gmail.com>
2022-02-27 13:11:03 +0300

    1. change deployment and service name 2. add CORS

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import uuid
 from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get('/', tags=['root'])
